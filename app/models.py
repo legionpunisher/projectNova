ed653b13cbbfcb049067cabea36ea3a635c3cb7e
legionpunisher <mbuguamoses656@gmail.com>
2020-11-04 21:34:28 +0300

    added hood model that will carry the neighbourhood details

--- app/models.py
+++ app/models.py
@@ -6,3 +6,9 @@ class Location(models.Model):
 
     def __str__(self):
         return self.name
+class Hood(models.Model):
+    hood_photo = models.ImageField(upload_to='hoods/')
+    hood_name = models.CharField(max_length=100, null=True)
+    occupants_count = models.PositiveIntegerField(default=0)
+    location = models.ForeignKey(Location, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
