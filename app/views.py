52a412516eec8bf153dcd184a91cc23dea855a5d
legionpunisher <mbuguamoses656@gmail.com>
2020-11-04 21:31:05 +0300

    created a hood location model

--- app/views.py
+++ app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+
+    if request.user.is_authenticated:
+        if Join.objects.filter(user_id=request.user).exists():
+            hood = Hood.objects.get(pk=request.user.join.hood_id.id)
+            posts = Posts.objects.filter(hood=request.user.join.hood_id.id)
+            businesses = Business.objects.filter(
+                hood=request.user.join.hood_id.id)
+
+            return render(request, 'hoods/hood.html', {"hood": hood, "businesses": businesses, "posts": posts})
+        else:
+            neighbourhoods = Hood.objects.all()
+            return render(request, 'index.html', {"neighbourhoods": neighbourhoods})
+    else:
+        neighbourhoods = Hood.objects.all()
+        return render(request, 'index.html', {"neighbourhoods": neighbourhoods})
+
