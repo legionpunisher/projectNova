a56b67ceb5c822c8f868431b7eeb9a93b278c7de
legionpunisher <mbuguamoses656@gmail.com>
2020-11-05 07:54:15 +0300

    added a database to our app

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework.authtoken',
+    'rest_framework',
+    'bootstrap4',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -75,11 +79,12 @@ WSGI_APPLICATION = 'neighbourhood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neigh',
+        'USER': 'falcon',
+    'PASSWORD':'shadow',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
